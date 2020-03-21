c69ecd50b575e4217610407665f8380d3dd9cd9d
Anwesh43 <anweshthecool0@gmail.com>
2020-03-21 19:12:04 +0530

    adding view and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbiballtoarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbiballtoarcview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbiballtoarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.biballtoarcview.BiBallToArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiBallToArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
