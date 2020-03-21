853436b02fbf0ae778bd619458f05f85320128cb
Anwesh43 <anweshthecool0@gmail.com>
2020-03-21 18:22:51 +0530

    adding constants and libraries

--- biballtoarcview/src/main/java/com/anwesh/uiprojects/biballtoarcview/BiBallToArcView.kt
+++ biballtoarcview/src/main/java/com/anwesh/uiprojects/biballtoarcview/BiBallToArcView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.biballtoarcview
 /**
  * Created by anweshmishra on 20/03/20.
  */
+
+
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val balls : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val scGap : Float = 0.02f
