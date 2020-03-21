7532865f7432721a70b357d18f94be6ee0110e7c
Anwesh43 <anweshthecool0@gmail.com>
2020-03-21 18:52:34 +0530

    adding BBTANode class

--- biballtoarcview/src/main/java/com/anwesh/uiprojects/biballtoarcview/BiBallToArcView.kt
+++ biballtoarcview/src/main/java/com/anwesh/uiprojects/biballtoarcview/BiBallToArcView.kt
@@ -133,4 +133,46 @@ class BiBallToArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBTANode(var i : Int, val state : State = State()) {
+
+        private var next : BBTANode? = null
+        private var prev : BBTANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BBTANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBTANode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBTANode {
+            var curr : BBTANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
