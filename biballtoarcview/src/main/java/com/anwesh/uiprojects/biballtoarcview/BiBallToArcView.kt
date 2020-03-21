55aced99ef053e0ca3238809def5420fb9a1c841
Anwesh43 <anweshthecool0@gmail.com>
2020-03-21 18:40:50 +0530

    adding Canvas extension method

--- biballtoarcview/src/main/java/com/anwesh/uiprojects/biballtoarcview/BiBallToArcView.kt
+++ biballtoarcview/src/main/java/com/anwesh/uiprojects/biballtoarcview/BiBallToArcView.kt
@@ -10,6 +10,7 @@ import android.app.Activity
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.view.View
 import android.view.MotionEvent
 
@@ -21,8 +22,49 @@ val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val scGap : Float = 0.02f
+val rFactor : Float = 6f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiBallToArc(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, balls)
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val sj : Float = 1f - 2 * i
+    val deg : Float = 180f * sf2
+    val r : Float = size / rFactor
+    val sx : Float = size * sf1 * sj
+    val startDeg : Float = 180f * i + 90f
+    save()
+    rotate(deg)
+    save()
+    paint.style = Paint.Style.FILL
+    drawCircle(0f, sx, r, paint)
+    restore()
+    paint.style = Paint.Style.STROKE
+    drawArc(RectF(-r, -r, r, r), startDeg, deg, false, paint)
+    restore()
+}
+
+fun Canvas.drawBiBallsToArc(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(balls - 1)) {
+        drawBiBallToArc(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawBBTANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBiBallsToArc(scale, size, paint)
+    restore()
+}
